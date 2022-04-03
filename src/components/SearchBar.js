b2f419fabb48323527262f3755cd130747e46cde
Ng Chan Kok <ngchankok@gmail.com>
2022-04-03 16:57:04 +0800

    add query function for BusArrival API

---
+++ src/components/SearchBar.js
@@ -0,0 +1,47 @@
+import API from './API'
+import { useState} from "react";
+
+export default function SearchBar(props) {
+    const [busStopCode, setBusStopCode] = useState('');
+    const [busNumber, setBusNumber] = useState('');
+
+    async function getBusArrivalData(e) {
+        e.preventDefault();
+        const RESPONSE = await API.get('/BusArrivalv2', {
+            headers:{
+                'AccountKey': 'HcXVAu/3TQ6F7Ymp1JHamA==',
+            },
+            params:{
+                'BusStopCode': `${busStopCode}`,
+                'ServiceNo': `${busNumber}`
+            }
+        });
+
+        let data = RESPONSE.data.Services
+        props.fetchData({data})
+
+    }
+
+    function handleInput(e) {
+        const FIELDNAME = e.target.name;
+
+        switch(FIELDNAME) {
+            case 'busStopCode':
+                setBusStopCode(e.target.value);
+                break;
+            case 'busNumber':
+                setBusNumber(e.target.value);
+                break;
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={getBusArrivalData}>
+                <input type='text' placeholder='Enter Bus Stop Code' name='busStopCode' onChange={handleInput} />
+                <input type='text' placeholder='Enter Bus Number' name='busNumber' onChange={handleInput} />
+                <button>Search</button>
+            </form>
+        </>
+    )
+}
